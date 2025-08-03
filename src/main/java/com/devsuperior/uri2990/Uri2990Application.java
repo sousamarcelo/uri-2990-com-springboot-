9af336ec8f51c7f274fee9f0457336deb7a211bf
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-08-02 23:11:07 -0300

    Teste retorno da consulta JPQL no programa principal/console

--- src/main/java/com/devsuperior/uri2990/Uri2990Application.java
+++ src/main/java/com/devsuperior/uri2990/Uri2990Application.java
@@ -27,10 +27,17 @@ public class Uri2990Application implements CommandLineRunner {
 		List<EmpregadoDeptProjection> list1 = repository.search1();
 		List<EmpregadoDeptDTO> result1 = list1.stream().map(x -> new EmpregadoDeptDTO(x)).collect(Collectors.toList());
 		
-		System.out.println("\n*** RESULTADO SQL RAIZ:");
+		System.out.println("\n*** RESULTADO SQL RAIZ NOT IN:");
 		for(EmpregadoDeptDTO obj : result1) {
 			System.out.println(obj);
 		}
 		System.out.println("\n\n");
+		
+		List<EmpregadoDeptDTO> result2 = repository.search2();
+		
+		System.out.println("\n*** RESULTADO JPQL:");
+		for(EmpregadoDeptDTO obj : result2) {
+			System.out.println(obj);
+		}
 	}
 }
